b600c539bfed25ccbf2bc9afb1dfc7f8878a2d73
Marco Lam <lamma3@oocl.com>
2020-04-14 15:03:35 +0800

    rename methods and variables

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,17 @@
+public class WordCount {
+    private String word;
+    private int count;
+
+    public WordCount(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+}
--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,35 +7,35 @@ public class WordFrequencyGame {
     private static final String SPACE_DELIMITER = " ";
     private static final String CALCULATE_ERROR_MESSAGE = "Calculate Error";
 
-    public String getResult(String inputStr) {
-        if (inputStr.split(SPACE_DELIMITER_PATTERN).length == 1) {
-            return inputStr + " 1";
+    public String getResult(String sentence) {
+        if (sentence.split(SPACE_DELIMITER_PATTERN).length == 1) {
+            return sentence + " 1";
         } else {
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split(SPACE_DELIMITER_PATTERN);
+                String[] sentenceSplitBySpace = sentence.split(SPACE_DELIMITER_PATTERN);
 
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
+                List<WordCount> wordCountList = new ArrayList<>();
+                for (String word : sentenceSplitBySpace) {
+                    WordCount wordCount = new WordCount(word, 1);
+                    wordCountList.add(wordCount);
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
+                Map<String, List<WordCount>> map = getListMap(wordCountList);
 
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
+                List<WordCount> list = new ArrayList<>();
+                for (Map.Entry<String, List<WordCount>> entry : map.entrySet()) {
+                    WordCount wordCount = new WordCount(entry.getKey(), entry.getValue().size());
+                    list.add(wordCount);
                 }
-                inputList = list;
+                wordCountList = list;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordCountList.sort((wordCount1, wordCount2) -> wordCount2.getCount() - wordCount1.getCount());
 
                 StringJoiner joiner = new StringJoiner(NEWLINE_DELIMITER);
-                for (Input w : inputList) {
-                    String s = w.getValue() + SPACE_DELIMITER + w.getWordCount();
+                for (WordCount w : wordCountList) {
+                    String s = w.getWord() + SPACE_DELIMITER + w.getCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
@@ -45,15 +45,15 @@ public class WordFrequencyGame {
         }
     }
 
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+    private Map<String, List<WordCount>> getListMap(List<WordCount> wordCountList) {
+        Map<String, List<WordCount>> map = new HashMap<>();
+        for (WordCount wordCount : wordCountList) {
+            if (!map.containsKey(wordCount.getWord())) {
+                List<WordCount> arr = new ArrayList<>();
+                arr.add(wordCount);
+                map.put(wordCount.getWord(), arr);
             } else {
-                map.get(input.getValue()).add(input);
+                map.get(wordCount.getWord()).add(wordCount);
             }
         }
         return map;
