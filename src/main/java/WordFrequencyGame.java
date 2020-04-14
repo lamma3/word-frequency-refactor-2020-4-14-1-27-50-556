219633192273e701b53b6f9204f4b18574a70fdb
Marco Lam <lamma3@oocl.com>
2020-04-14 15:08:37 +0800

    extract functions

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,41 +8,39 @@ public class WordFrequencyGame {
     private static final String CALCULATE_ERROR_MESSAGE = "Calculate Error";
 
     public String getResult(String sentence) {
-        if (sentence.split(SPACE_DELIMITER_PATTERN).length == 1) {
-            return sentence + " 1";
-        } else {
-            try {
-                //split the input string with 1 to n pieces of spaces
-                String[] sentenceSplitBySpace = sentence.split(SPACE_DELIMITER_PATTERN);
-
-                List<WordCount> wordCountList = new ArrayList<>();
-                for (String word : sentenceSplitBySpace) {
-                    WordCount wordCount = new WordCount(word, 1);
-                    wordCountList.add(wordCount);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordCount>> map = getListMap(wordCountList);
-
-                List<WordCount> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordCount>> entry : map.entrySet()) {
-                    WordCount wordCount = new WordCount(entry.getKey(), entry.getValue().size());
-                    list.add(wordCount);
-                }
-                wordCountList = list;
-
-                wordCountList.sort((wordCount1, wordCount2) -> wordCount2.getCount() - wordCount1.getCount());
-
-                StringJoiner joiner = new StringJoiner(NEWLINE_DELIMITER);
-                for (WordCount w : wordCountList) {
-                    String s = w.getWord() + SPACE_DELIMITER + w.getCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return CALCULATE_ERROR_MESSAGE;
-            }
+        try {
+            String[] sentenceSplitBySpace = splitBySpace(sentence);
+            List<WordCount> wordCountList = calculateWordCount(sentenceSplitBySpace);
+            return formatWordCountResult(wordCountList);
+        } catch (Exception e) {
+            return CALCULATE_ERROR_MESSAGE;
         }
+    }
+
+    private String[] splitBySpace(String sentence) {
+        //split the input string with 1 to n pieces of spaces
+        return sentence.split(SPACE_DELIMITER_PATTERN);
+    }
+
+    private List<WordCount> calculateWordCount(String[] sentenceSplitBySpace) {
+        List<WordCount> wordCountList = new ArrayList<>();
+        for (String word : sentenceSplitBySpace) {
+            WordCount wordCount = new WordCount(word, 1);
+            wordCountList.add(wordCount);
+        }
+
+        //get the map for the next step of sizing the same word
+        Map<String, List<WordCount>> map = getListMap(wordCountList);
+
+        List<WordCount> list = new ArrayList<>();
+        for (Map.Entry<String, List<WordCount>> entry : map.entrySet()) {
+            WordCount wordCount = new WordCount(entry.getKey(), entry.getValue().size());
+            list.add(wordCount);
+        }
+        wordCountList = list;
+
+        wordCountList.sort((wordCount1, wordCount2) -> wordCount2.getCount() - wordCount1.getCount());
+        return wordCountList;
     }
 
     private Map<String, List<WordCount>> getListMap(List<WordCount> wordCountList) {
@@ -57,5 +55,14 @@ public class WordFrequencyGame {
             }
         }
         return map;
+    }
+
+    private String formatWordCountResult(List<WordCount> wordCountList) {
+        StringJoiner joiner = new StringJoiner(NEWLINE_DELIMITER);
+        for (WordCount w : wordCountList) {
+            String s = w.getWord() + SPACE_DELIMITER + w.getCount();
+            joiner.add(s);
+        }
+        return joiner.toString();
     }
 }
